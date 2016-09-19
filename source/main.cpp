349771c27711e3e6d551840e1cffde214f093930
David Erbelding <dje3873@rit.edu>
2016-09-19 10:44:44 -0400

    dt based movement

--- source/main.cpp
+++ source/main.cpp
@@ -163,13 +163,18 @@ int main(int argc, char **argv)
 	// Main Loop
 	while (!glfwWindowShouldClose(window))
 	{
+        // Calculate delta time.
+        float dt = glfwGetTime();
+        // Reset the timer.
+        glfwSetTime(0);
+
 		// Clear the screen.
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.0, 0.0, 0.0, 0.0);
 
 
 		// rotate square
-		transform.Rotate(.0002f);
+		transform.Rotate(dt);
 		
 		// Set the current shader program.
 		glUseProgram(shaderProgram);
